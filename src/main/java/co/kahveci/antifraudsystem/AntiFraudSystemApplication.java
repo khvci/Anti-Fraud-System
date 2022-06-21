4042fda8d98d17b602476339d18a285a1a35dd16
khvci <88493840+khvci@users.noreply.github.com>
2022-06-21 21:26:31 +0300

    Initial commit

---
+++ src/main/java/co/kahveci/antifraudsystem/AntiFraudSystemApplication.java
@@ -0,0 +1,13 @@
+package co.kahveci.antifraudsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFraudSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntiFraudSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/kahveci/antifraudsystem/AntiFraudSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kahveci.antifraudsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiFraudSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
