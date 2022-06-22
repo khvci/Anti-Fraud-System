db0a50640e2841594ae15d107dff23cf0a7726d1
khvci <88493840+khvci@users.noreply.github.com>
2022-06-22 19:14:29 +0300

    add project overview screenshot

--- src/main/java/co/kahveci/antifraudsystem/TransactionService.java
+++ src/main/java/co/kahveci/antifraudsystem/TransactionService.java
@@ -3,7 +3,6 @@ package co.kahveci.antifraudsystem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.util.Map;
 
 @Service
